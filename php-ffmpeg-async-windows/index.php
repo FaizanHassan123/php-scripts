b845ad704942f108baa669b9863a1b0ceb857996
faizanhassan <fh162556@gmail.com>
2022-12-11 21:33:49 +0500

    .

--- php-ffmpeg-async-windows/index.php
+++ php-ffmpeg-async-windows/index.php
@@ -4,8 +4,7 @@ $start_time = microtime(true);
 
 
 // Starting a Background Script
-pclose(popen("start /B run
-.bat 3.mp4 3out.mp4", "r"));
+pclose(popen("start /B run.bat 3.mp4 3out.mp4", "r"));
 
 
 // End Time
